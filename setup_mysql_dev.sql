53b1c01e3be00ef0c7221574f7f7745903841b06
Chura33 <churabrady@gmail.com>
2023-09-19 13:08:08 -0700

    solved task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,18 @@
--- creates a database hbnb_dev_db
--- this database is the database for our AirBnB site
+-- Project SQL Script
+-- Description: Prepare MySQL server for project
+
+-- Create the database hbnb_dev_db if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create user if not exists
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'CHUrabrady123?';
+-- Create the user hbnb_dev if it doesn't already exist
+CREATE USER IF NOT EXISTS 'chura'@'localhost' IDENTIFIED BY CHUrabrady123?';
 
--- Grant all privileges on hbnb_dev_db touser hbhb_dev
+-- Grant all privileges on hbnb_dev_db to the user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Grant SELECT privilege on performance_schema to user hbnb_dev
+-- Grant SELECT privilege on performance_schema to the user hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
+
