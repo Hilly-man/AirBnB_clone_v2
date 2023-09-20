5092805fea0243eb3ed6893236c763c064fb7601
Chura33 <churabrady@gmail.com>
2023-09-19 23:50:29 -0700

    solved task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,23 @@
+-- check if the database hbnb_test_db exists
+SELECT SCHEMA_NAME
+FROM INFORMATION_SCHEMA.SCHEMATA
+WHERE SCHEMA_NAME = 'hbnb_test_db';
+
+-- If the database doesn't exist, create it
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+
+-- check if the user hbnb_test exists
+SELECT user
+FROM mysql.user
+WHERE user = 'hbnb_test';
+
+
+-- If the user doesnt exist, create it and grant privileges
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to the user hbnb_test on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
