7c97d3d320ad9005111be9040399d6b5be3365c1
Chura33 <churabrady@gmail.com>
2023-10-22 06:17:20 -0700

    solved task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application:
-
-   - Your web application must be listening on 0.0.0.0, port 5000
-   - Routes:
-   - /: display “Hello HBNB!”
-   - /hbnb: display “HBNB”
-   - You must use the option strict_slashes=False in your route definition
 """
-
-
-from flask import Flask
+Script that starts a Flask web application:
+    listening on 0.0.0.0, port 5000
+    With two Routes
+    """
+from flask import Flask, escape, request
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """ This function displaysHello HBNB"""
+    """ First Route that display Hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
-    """ This function displays Hello HBNB!"""
-    return "HBNB"
+    """ Second Route that display HBNB"""
+        return "HBNB"
 
-
-if __name__ == "_main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
