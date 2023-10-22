37485e0722a99a718cb9786852baf7fb74e87dbf
Chura33 <churabrady@gmail.com>
2023-10-22 05:23:02 -0700

    solved task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,17 @@
 """ This module is a flask app that displys Hello HBNB when visited"""
 
 from flask import Flask
-
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def hello():
     """ This function displaysHello HBNB"""
     return 'Hello HBNB!'
+
+
 if __name__ == "__main__":
-""" To Start a New Web Application """
-from flask import Flask
+    """ To Start a New Web Application """
 app = Flask(__name__)
 
 
@@ -18,6 +20,7 @@ app = Flask(__name__)
 def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     """ Main Function """
